6c9a13547845f14c5dfc7b1a9804093cedada63f
waynebeam <106614612+waynebeam@users.noreply.github.com>
2023-03-24 00:50:43 +0000

    Can use custom link to access db and say hi to the name

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import psycopg2
 
 db_string = os.environ['DB_CONNECTION']
 
-password = os.environ['PASSWORD']
+correct_password = os.environ['PASSWORD']
 
 def test_db_access():
   with psycopg2.connect(db_string) as conn:
@@ -12,3 +12,14 @@ def test_db_access():
       curr.execute(sql)
       print(curr.fetchone())
 
+
+def validate_user(nickname, password):
+  if password != correct_password:
+    return False
+  with psycopg2.connect(db_string) as conn:
+    with conn.cursor() as curr:
+      sql = 'SELECT name FROM guests WHERE nickname = %s'
+      curr.execute(sql, [nickname])
+      return curr.fetchone()
+
+print(validate_user("wayne", "archparty")[0])
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from database import validate_user
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+    return "Archer's Blue's Clues Party!"
 
+@app.route("/invite/<nickname>/<password>")
+def show_invite(nickname, password):
+  response = validate_user(nickname, password)
+  if response:
+    return f'Hello {response[0]}!'
+  return 'invalid link'
+  
 
 app.run(host='0.0.0.0', port=81)
